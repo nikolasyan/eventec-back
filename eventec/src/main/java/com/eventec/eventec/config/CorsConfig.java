81d1e603ae4efae95ef1dde75dacbb71c3786404
nikolasyan <nikolas.oliveira0701@gmail.com>
2024-11-02 21:51:14 -0300

    dockerfile

--- eventec/src/main/java/com/eventec/eventec/config/CorsConfig.java
+++ eventec/src/main/java/com/eventec/eventec/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "http://10.0.2.2:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
